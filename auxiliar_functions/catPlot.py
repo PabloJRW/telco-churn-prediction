d3feebcd9b8ad037f0b20f7f2f7d2c4065531023
PabloJRW <ramoswilkinspj28@gmail.com>
2022-09-21 15:32:40 -0500

    desarrollo eda

---
+++ auxiliar_functions/catPlot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def catPlot(df, categorical):
+    fig, ax = plt.subplots(nrows=5, ncols=3, figsize=(18,18))
+    ax = ax.flatten()
+    plt.suptitle('Categorical Variables', fontweight='bold', fontsize=24)
+
+    for idx, var in enumerate(categorical):
+        # Distribution Plots
+        sns.countplot(x=df[var], hue=df.churn, ax=ax[idx])
+        ax[idx].set_title((var + ' distribution').upper())
+        ax[idx].set_xlabel(None)
+        
+    fig.tight_layout(h_pad=3)
